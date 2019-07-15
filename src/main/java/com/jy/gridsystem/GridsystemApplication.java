28a88e8cebba176506960e28c08d71c21c4adfde
Justin <justinyang1000@hotmail.com>
2019-07-15 09:55:25 -0400

    Initial commit

---
+++ src/main/java/com/jy/gridsystem/GridsystemApplication.java
@@ -0,0 +1,13 @@
+package com.jy.gridsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GridsystemApplication.class, args);
+    }
+
+}
